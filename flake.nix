e1b11b97b993e2bdbface0cc4568b56da65af649
David Baynard <git@baynard.dev>
2023-09-27 21:46:08 +0100

    Create flake for aarch64-darwin

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Postgrest";
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      extraOverrides = final: prev: {
+        http2 = pkgs.haskell.lib.dontCheck prev.http2;
+      };
+      p = import ./. {
+        inherit nixpkgs extraOverrides;
+        inherit (pkgs) system;
+      };
+    in
+    {
+      packages.aarch64-darwin.default = lib.pipe p.postgrestPackage
+        [ pkgs.haskell.lib.justStaticExecutables ];
+    };
+}
